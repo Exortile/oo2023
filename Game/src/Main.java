3db36c289717055189f46ff3eac6f8ea3f58eb65
Exortile <xortile@gmail.com>
2023-02-17 08:17:41 -0500

    2. nädala lõpp: Klassid

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -22,14 +22,13 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        int worldWidth = 10;
-        int worldHeight = 5;
+        World world = new World(10, 5);
 
-        Player player = new Player(worldWidth, worldHeight);
-        Dragon dragon = new Dragon(worldWidth, worldHeight);
-        Orc orc = new Orc(worldWidth, worldHeight);
+        Player player = new Player(world.width, world.height);
+        Dragon dragon = new Dragon(world.width, world.height);
+        Orc orc = new Orc(world.width, world.height);
 
-        printMap(worldWidth, worldHeight,
+        world.printMap(world.width, world.height,
                 player.xCoordinate, player.yCoordinate, player.symbol,
                 dragon.xCoordinate, dragon.yCoordinate, dragon.symbol,
                 orc.xCoordinate, orc.yCoordinate, orc.symbol);
@@ -38,53 +37,15 @@ public class Main {
         String input = scanner.nextLine();
 
         while (!input.equals("end")) {
-            player.move(input);
+            player.move(input, world);
 
-            printMap(worldWidth, worldHeight,
+            world.printMap(world.width, world.height,
                     player.xCoordinate, player.yCoordinate, player.symbol,
                     dragon.xCoordinate, dragon.yCoordinate, dragon.symbol,
                     orc.xCoordinate, orc.yCoordinate, orc.symbol);
 
             System.out.println();
             input = scanner.nextLine();
-        }
-    }
-
-    private static void printMap(int worldWidth, int worldHeight, int playerXCoordinate, int playerYCoordinate, char playerSymbol, int dragonXCoordinate, int dragonYCoordinate, char dragonSymbol, int orcXCoordinate, int orcYCoordinate, char orcSymbol) {
-        // algväärtus   kuni   iga tsükkel
-        for (int y = 0; y < worldHeight; y++) {
-            System.out.println();
-            for (int x = 0; x < worldWidth; x++) {
-                if (y == 0 || y == worldHeight - 1) {
-                    System.out.print("-");
-                } else if (x == 0 || x == worldWidth - 1) {
-                    System.out.print("|");
-                } else {
-                    // ctrl + alt + m
-                    // right click -> refactor -> extract method
-                    printCharacters(playerXCoordinate, playerYCoordinate, playerSymbol,
-                            dragonXCoordinate, dragonYCoordinate, dragonSymbol,
-                            orcXCoordinate, orcYCoordinate, orcSymbol, y, x);
-                }
-            }
-        }
-    }
-
-    private static int getRandomCoordinate(int worldDimension) {
-        return (int) (Math.random() * (worldDimension - 2) + 1);
-    }
-
-    private static void printCharacters(int playerXCoordinate, int playerYCoordinate, char playerSymbol,
-                                        int dragonXCoordinate, int dragonYCoordinate, char dragonSymbol,
-                                        int orcXCoordinate, int orcYCoordinate, char orcSymbol, int mapY, int mapX) {
-        if (playerXCoordinate == mapX && playerYCoordinate == mapY) {
-            System.out.print(playerSymbol);
-        } else if (dragonXCoordinate == mapX && dragonYCoordinate == mapY) {
-            System.out.print(dragonSymbol);
-        } else if (orcXCoordinate == mapX && orcYCoordinate == mapY) {
-            System.out.print(orcSymbol);
-        } else {
-            System.out.print(" ");
         }
     }
 }
--- Game/src/Player.java
+++ Game/src/Player.java
@@ -2,23 +2,44 @@ public class Player {
     int xCoordinate; // taisarvuline number
     int yCoordinate;
     char symbol;
+    Direction direction;
 
     public Player(int worldWidth, int worldHeight) {
         this.xCoordinate = getRandomCoordinate(worldWidth);
         this.yCoordinate = getRandomCoordinate(worldHeight);
         this.symbol = 'X';
+        this.direction = Direction.UP;
     }
 
     private static int getRandomCoordinate(int worldDimension) {
         return (int) (Math.random() * (worldDimension - 2) + 1);
     }
 
-    public void move(String input) {
+    public void move(String input, World world) {
         switch (input) {
-            case "w" -> this.yCoordinate -= 1;
-            case "s" -> this.yCoordinate += 1;
-            case "a" -> this.xCoordinate -= 1;
-            case "d" -> this.xCoordinate += 1;
+            case "w" -> this.direction = Direction.UP;
+            case "s" -> this.direction = Direction.DOWN;
+            case "a" -> this.direction = Direction.LEFT;
+            case "d" -> this.direction = Direction.RIGHT;
+        }
+
+        switch (this.direction) {
+            case UP -> {
+                if (this.yCoordinate > 1)
+                    this.yCoordinate -= 1;
+            }
+            case DOWN -> {
+                if (this.yCoordinate < world.height - 2)
+                    this.yCoordinate += 1;
+            }
+            case LEFT -> {
+                if (this.xCoordinate > 1)
+                    this.xCoordinate -= 1;
+            }
+            case RIGHT -> {
+                if (this.xCoordinate < world.width - 2)
+                    this.xCoordinate += 1;
+            }
         }
     }
 }
