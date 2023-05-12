5913126fe78c89ed8d3305b82e2b300c479dfa30
Exortile <xortile@gmail.com>
2023-05-12 05:23:59 -0400

    proovikontrolltoo 2

---
+++ 11_proovikontrolltoo2/proovikontrolltoo/src/test/java/ee/sten/proovikontrolltoo/ProovikontrolltooApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.sten.proovikontrolltoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProovikontrolltooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
